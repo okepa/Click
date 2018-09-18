30eb8c58901d6cb5e4dd9dbc549d374474349205
aleksander.kepa <aleksander.kepa@gwle.onmicrosoft.com>
2018-09-18 14:42:29 +0100

    First init

---
+++ Click/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Click
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.intervalLowerBoundTextBox = new System.Windows.Forms.TextBox();
+            this.intervalLowerBoundLabel = new System.Windows.Forms.Label();
+            this.intervalUpperBoundLabel = new System.Windows.Forms.Label();
+            this.intervalUpperBoundTextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.countButton = new System.Windows.Forms.Button();
+            this.countTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(152, 24);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(100, 22);
+            this.yTextBox.TabIndex = 1;
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(46, 24);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 22);
+            this.xTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "y";
+            // 
+            // intervalLowerBoundTextBox
+            // 
+            this.intervalLowerBoundTextBox.Location = new System.Drawing.Point(46, 107);
+            this.intervalLowerBoundTextBox.Name = "intervalLowerBoundTextBox";
+            this.intervalLowerBoundTextBox.Size = new System.Drawing.Size(100, 22);
+            this.intervalLowerBoundTextBox.TabIndex = 5;
+            // 
+            // intervalLowerBoundLabel
+            // 
+            this.intervalLowerBoundLabel.AutoSize = true;
+            this.intervalLowerBoundLabel.Location = new System.Drawing.Point(43, 78);
+            this.intervalLowerBoundLabel.Name = "intervalLowerBoundLabel";
+            this.intervalLowerBoundLabel.Size = new System.Drawing.Size(141, 17);
+            this.intervalLowerBoundLabel.TabIndex = 6;
+            this.intervalLowerBoundLabel.Text = "Interval Lower Bound";
+            // 
+            // intervalUpperBoundLabel
+            // 
+            this.intervalUpperBoundLabel.AutoSize = true;
+            this.intervalUpperBoundLabel.Location = new System.Drawing.Point(202, 78);
+            this.intervalUpperBoundLabel.Name = "intervalUpperBoundLabel";
+            this.intervalUpperBoundLabel.Size = new System.Drawing.Size(142, 17);
+            this.intervalUpperBoundLabel.TabIndex = 7;
+            this.intervalUpperBoundLabel.Text = "Interval Upper Bound";
+            // 
+            // intervalUpperBoundTextBox
+            // 
+            this.intervalUpperBoundTextBox.Location = new System.Drawing.Point(205, 107);
+            this.intervalUpperBoundTextBox.Name = "intervalUpperBoundTextBox";
+            this.intervalUpperBoundTextBox.Size = new System.Drawing.Size(100, 22);
+            this.intervalUpperBoundTextBox.TabIndex = 8;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(361, 107);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 9;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // countButton
+            // 
+            this.countButton.Location = new System.Drawing.Point(46, 198);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(75, 23);
+            this.countButton.TabIndex = 10;
+            this.countButton.Text = "Count";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // countTextBox
+            // 
+            this.countTextBox.Location = new System.Drawing.Point(137, 198);
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.Size = new System.Drawing.Size(100, 22);
+            this.countTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 417);
+            this.Controls.Add(this.countTextBox);
+            this.Controls.Add(this.countButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.intervalUpperBoundTextBox);
+            this.Controls.Add(this.intervalUpperBoundLabel);
+            this.Controls.Add(this.intervalLowerBoundLabel);
+            this.Controls.Add(this.intervalLowerBoundTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox intervalLowerBoundTextBox;
+        private System.Windows.Forms.Label intervalLowerBoundLabel;
+        private System.Windows.Forms.Label intervalUpperBoundLabel;
+        private System.Windows.Forms.TextBox intervalUpperBoundTextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button countButton;
+        private System.Windows.Forms.TextBox countTextBox;
+    }
+}
+
---
+++ Click/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Click
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // P/Invoke function for controlling the mouse
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+
+        /// <summary>
+        /// structure for mouse data
+        /// </summary>
+        struct MouseInput
+        {
+            public int X; // X coordinate
+            public int Y; // Y coordinate
+            public uint MouseData; // mouse data, e.g. for mouse wheel
+            public uint DwFlags; // further mouse data, e.g. for mouse buttons
+            public uint Time; // time of the event
+            public IntPtr DwExtraInfo; // further information
+        }
+
+        /// <summary>
+        /// super structure for input data of the function SendInput
+        /// </summary>
+        struct Input
+        {
+            public int Type; // type of the input, 0 for mouse  
+            public MouseInput Data; // mouse data
+        }
+
+        int count = 0;
+
+        // constants for mouse flags
+        const uint MOUSEEVENTF_LEFTDOWN = 0x0002; // press left mouse button
+        const uint MOUSEEVENTF_LEFTUP = 0x0004; // release left mouse button
+        const uint MOUSEEVENTF_ABSOLUTE = 0x8000; // whole screen, not just application window
+        const uint MOUSEEVENTF_MOVE = 0x0001; // move mouse
+
+        private MouseInput CreateMouseInput(int x, int y, uint data, uint time, uint flag)
+        {
+            // create from the given data an object of the type MouseInput, which then can be send
+            MouseInput Result = new MouseInput();
+            Result.X = x;
+            Result.Y = y;
+            Result.MouseData = data;
+            Result.Time = time;
+            Result.DwFlags = flag;
+            return Result;
+        }
+
+        private void SimulateMouseClick()
+        {
+            // Linksklick simulieren: Maustaste drücken und loslassen
+            Input[] MouseEvent = new Input[2];
+            MouseEvent[0].Type = 0;
+            MouseEvent[0].Data = CreateMouseInput(0, 0, 0, 0, MOUSEEVENTF_LEFTDOWN);
+
+            MouseEvent[1].Type = 0; // INPUT_MOUSE; 
+            MouseEvent[1].Data = CreateMouseInput(0, 0, 0, 0, MOUSEEVENTF_LEFTUP);
+
+            SendInput((uint)MouseEvent.Length, MouseEvent, Marshal.SizeOf(MouseEvent[0].GetType()));
+        }
+
+        //private void IntervalLeftClick(int lowerBound, int upperBound)
+        //{
+        //    Random rnd = new Random();
+        //    int wait;
+        //    // Linksklick simulieren: Maustaste drücken und loslassen
+        //    Input[] MouseEvent = new Input[2];
+        //    MouseEvent[0].Type = 0;
+        //    MouseEvent[0].Data = CreateMouseInput(0, 0, 0, 0, MOUSEEVENTF_LEFTDOWN);
+
+        //    MouseEvent[1].Type = 0; // INPUT_MOUSE; 
+        //    MouseEvent[1].Data = CreateMouseInput(0, 0, 0, 0, MOUSEEVENTF_LEFTUP);
+
+        //    wait = rnd.Next(lowerBound, upperBound) * 1000;
+        //    SendInput((uint)MouseEvent.Length, MouseEvent, Marshal.SizeOf(MouseEvent[0].GetType()));
+        //    Thread.Sleep(wait);
+        //}
+
+        private void SimulateMouseMove(int x, int y)
+        {
+            Input[] MouseEvent = new Input[1];
+            MouseEvent[0].Type = 0;
+            // move mouse: Flags ABSOLUTE (whole screen) and MOVE (move)
+            MouseEvent[0].Data = CreateMouseInput(x, y, 0, 0, MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE);
+            SendInput((uint)MouseEvent.Length, MouseEvent, Marshal.SizeOf(MouseEvent[0].GetType()));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int32.TryParse(xTextBox.Text, out int x);
+            Int32.TryParse(yTextBox.Text, out int y);
+            // move mouse
+            SimulateMouseMove(x, y);
+            // click
+            SimulateMouseClick();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            ThreadStart intervalRef = new ThreadStart(intervalStart);
+            Thread countThread = new Thread(intervalRef);
+            countThread.Start();
+            Thread.Sleep(5000);
+        }
+
+        private void intervalStart()
+        {
+            Random rnd = new Random();
+            int wait;
+            int count = 0;
+            Boolean loop = true;
+
+            Input[] MouseEvent = new Input[2];
+            MouseEvent[0].Type = 0;
+            MouseEvent[0].Data = CreateMouseInput(0, 0, 0, 0, MOUSEEVENTF_LEFTDOWN);
+
+            MouseEvent[1].Type = 0; // INPUT_MOUSE; 
+            MouseEvent[1].Data = CreateMouseInput(0, 0, 0, 0, MOUSEEVENTF_LEFTUP);
+
+            Console.WriteLine("Start interval thread");
+            Int32.TryParse(intervalLowerBoundLabel.Text, out int lowerBound);
+            Int32.TryParse(intervalUpperBoundTextBox.Text, out int upperBound);
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            while (loop)
+            {
+                Console.WriteLine(count);
+                wait = rnd.Next(lowerBound, upperBound) * 1000;
+                SendInput((uint)MouseEvent.Length, MouseEvent, Marshal.SizeOf(MouseEvent[0].GetType()));
+                Thread.Sleep(wait);
+                count++;
+                if (X != Cursor.Position.X || Y != Cursor.Position.Y)
+                {
+                    loop = false;
+                }
+            }
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            count++;
+            countTextBox.Text = count.ToString();
+        }    
+    }
+}
